88e8280601cb4f8cff8437ab5bebcfef51079b12
benmarita96@gmail.com <benmarita96@gmail.com>
2022-08-24 14:43:07 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
 
 const titleCased = () => {
   return tutorials
 }
+  return tutorials.map(function(words){
+    const arrayOfSentences= words.split(" ")
+      const arrayOfCapitalizedSentences = arrayOfSentences.map(function(wording){
+        return wording[0].toUpperCase()+ wording.slice(1)
+      })
+        const titleCasedSentence = arrayOfCapitalizedSentences.join(" ")
+          return titleCasedSentence
+  })
+  
+    titleCased();
